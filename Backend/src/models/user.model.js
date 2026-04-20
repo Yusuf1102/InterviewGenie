0cb878da8feeb934e36f4cffa0a66aafb7c7498e
Yusuf Saiyad <yusufsaiyed66@gmail.com>
2026-04-20 18:38:15 +0530

    created user schema

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        unique: [ true, 'Username already exists. Please choose another one.'],
+        required: [ true, 'Username is required.']
+    },
+    email: {
+        type: String,
+        unique: [ true, 'Email already exists. Please choose another one.'],
+        required: [ true, 'Email is required.'] 
+    },
+    password: {
+        type: String,
+        required: [ true, 'Password is required.']
+    }
+}, { timestamps: true });   
+    
+})
