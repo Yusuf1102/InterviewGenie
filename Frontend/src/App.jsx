a4dfc2b3d5d3ded33dc7a27bc1d50d3630c1d157
Yusuf Saiyad <yusufsaiyed66@gmail.com>
2026-04-21 16:10:19 +0530

    Login and Register Setup initialzed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './app.routes.jsx'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
