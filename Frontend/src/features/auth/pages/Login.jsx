22ce8e32edf9f7ed6eac3e69053e23d728f2629a
Yusuf Saiyad <yusufsaiyed66@gmail.com>
2026-04-23 16:22:57 +0530

    Authentication Form Styling & Architecture Overview

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { useNavigate,Link } from 'react-router-dom'
 import "../auth.form.scss"
+import "../../../style/button.scss"
 
 const Login = () => {
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    }   
+
+
+
   return (
    <main>
     <div className="form-container">
         <h1>Login</h1>
 
-        <form >
+        <form onSubmit={handleSubmit}>
 
             <div className="input-group">
                 <label htmlFor="email">Email</label>
@@ -19,7 +31,7 @@ const Login = () => {
                 <input type="password" id="password" name="password" placeholder='Enter your password' required />
             </div>
             <button className='button primary-button'>Login</button>
-
+            <p>Dont have an account?<Link to={"/register"}> Register</Link></p>
         </form>
     </div>
    </main>
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useNavigate,Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+    }
   return (
-    <div>
-      Register
+      <main>
+    <div className="form-container">
+        <h1>Register</h1>
+
+        <form onSubmit={handleSubmit}>
+
+            <div className="input-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" name="username" placeholder='Enter your username' required />
+            </div>
+            <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder='Enter your email' required />
+                        
+            </div>
+            <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name="password" placeholder='Enter your password' required />
+            </div>
+            <button className='button primary-button'>Register</button>
+        <p>Already have an account?<Link to={"/login"}> Login</Link></p>
+        </form>
     </div>
+   </main>
   )
 }
 
